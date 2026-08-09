1d8f7521ebca5c8c14e0a18868bfc104fb4e1db4
4p5w992jkp-cyber <4p5w992jkp@privaterelay.appleid.com>
2026-08-09 03:22:35 +0300

    Create protection.js

---
+++ commands/protection.js
@@ -0,0 +1,42 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require("discord.js");
+
+module.exports = [
+  {
+    data: new SlashCommandBuilder()
+      .setName("lock")
+      .setDescription("قفل القناة الحالية")
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
+    async execute(interaction) {
+      await interaction.channel.permissionOverwrites.edit(
+        interaction.guild.roles.everyone,
+        {
+          SendMessages: false
+        }
+      );
+
+      await interaction.reply("🔒 تم قفل القناة.");
+    }
+  },
+
+  {
+    data: new SlashCommandBuilder()
+      .setName("unlock")
+      .setDescription("فتح القناة الحالية")
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
+    async execute(interaction) {
+      await interaction.channel.permissionOverwrites.edit(
+        interaction.guild.roles.everyone,
+        {
+          SendMessages: null
+        }
+      );
+
+      await interaction.reply("🔓 تم فتح القناة.");
+    }
+  }
+];
